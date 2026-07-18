988bb9b5d19a4a432c08942d5e3c5f6e5da87a40
Aaron Ivie <aaron.ivie@lumeadigital.com>
2026-07-18 10:15:47 -0600

    mod taps only on opposite hand

--- config/miryoku/miryoku_babel/miryoku_layer_alternatives.h
+++ config/miryoku/miryoku_babel/miryoku_layer_alternatives.h
@@ -8,19 +8,19 @@
 
 #define MIRYOKU_ALTERNATIVES_BASE_COLEMAK \
 &kp Q,             &kp W,             &kp F,             &kp P,             &kp G,             &kp J,             &kp L,             &kp U,             &kp Y,             &kp SQT,           \
-U_MT(LGUI, A),     U_MT(LALT, R),     U_MT(LCTRL, S),    U_MT(LSHFT, T),    &kp D,             &kp H,             U_MT(LSHFT, N),    U_MT(LCTRL, E),    U_MT(LALT, I),     U_MT(LGUI, O),     \
+U_MT_LEFT(LGUI, A), U_MT_LEFT(LALT, R), U_MT_LEFT(LCTRL, S), U_MT_LEFT(LSHFT, T), &kp D,         &kp H,             U_MT_RIGHT(LSHFT, N), U_MT_RIGHT(LCTRL, E), U_MT_RIGHT(LALT, I), U_MT_RIGHT(LGUI, O), \
 U_LT(U_BUTTON, Z), U_MT(RALT, X),     &kp C,             &kp V,             &kp B,             &kp K,             &kp M,             &kp COMMA,         U_MT(RALT, DOT),   U_LT(U_BUTTON, SLASH),\
 U_NP,              U_NP,              U_LT(U_MEDIA, ESC),U_LT(U_NAV, SPACE),U_LT(U_MOUSE, TAB),U_LT(U_SYM, RET),  U_LT(U_NUM, BSPC), U_LT(U_FUN, DEL),  U_NP,              U_NP
 
 #define MIRYOKU_ALTERNATIVES_BASE_COLEMAKDH \
 &kp Q,             &kp W,             &kp F,             &kp P,             &kp B,             &kp J,             &kp L,             &kp U,             &kp Y,             &kp SQT,           \
-U_MT(LCTRL, A),    U_MT(LALT, R),     U_MT(LGUI, S),     U_MT(LSHFT, T),    &kp G,             &kp M,             U_MT(LSHFT, N),    U_MT(LGUI, E),     U_MT(LALT, I),     U_MT(LCTRL, O),    \
+U_MT_LEFT(LCTRL, A), U_MT_LEFT(LALT, R), U_MT_LEFT(LGUI, S), U_MT_LEFT(LSHFT, T), &kp G,         &kp M,             U_MT_RIGHT(LSHFT, N), U_MT_RIGHT(LGUI, E), U_MT_RIGHT(LALT, I), U_MT_RIGHT(LCTRL, O), \
 U_LT(U_BUTTON, Z), U_MT(RALT, X),     &kp C,             &kp D,             &kp V,             &kp K,             &kp H,             &kp COMMA,         U_MT(RALT, DOT),   U_LT(U_BUTTON, SLASH),\
 U_NP,              U_NP,              U_LT(U_MEDIA, ESC),U_LT(U_NAV, SPACE),U_LT(U_MOUSE, TAB),U_LT(U_SYM, RET),  U_LT(U_NUM, BSPC), U_LT(U_FUN, DEL),  U_NP,              U_NP
 
 #define MIRYOKU_ALTERNATIVES_BASE_COLEMAKDHK \
 &kp Q,             &kp W,             &kp F,             &kp P,             &kp B,             &kp J,             &kp L,             &kp U,             &kp Y,             &kp SQT,           \
-U_MT(LGUI, A),     U_MT(LALT, R),     U_MT(LCTRL, S),    U_MT(LSHFT, T),    &kp G,             &kp K,             U_MT(LSHFT, N),    U_MT(LCTRL, E),    U_MT(LALT, I),     U_MT(LGUI, O),     \
+U_MT_LEFT(LGUI, A), U_MT_LEFT(LALT, R), U_MT_LEFT(LCTRL, S), U_MT_LEFT(LSHFT, T), &kp G,         &kp K,             U_MT_RIGHT(LSHFT, N), U_MT_RIGHT(LCTRL, E), U_MT_RIGHT(LALT, I), U_MT_RIGHT(LGUI, O), \
 U_LT(U_BUTTON, Z), U_MT(RALT, X),     &kp C,             &kp D,             &kp V,             &kp M,             &kp H,             &kp COMMA,         U_MT(RALT, DOT),   U_LT(U_BUTTON, SLASH),\
 U_NP,              U_NP,              U_LT(U_MEDIA, ESC),U_LT(U_NAV, SPACE),U_LT(U_MOUSE, TAB),U_LT(U_SYM, RET),  U_LT(U_NUM, BSPC), U_LT(U_FUN, DEL),  U_NP,              U_NP
 
--- config/miryoku/miryoku_behaviors.h
+++ config/miryoku/miryoku_behaviors.h
@@ -4,4 +4,6 @@
 #pragma once
 
 #define U_MT(MOD, TAP) &u_mt MOD TAP
+#define U_MT_LEFT(MOD, TAP) &u_mt_left MOD TAP
+#define U_MT_RIGHT(MOD, TAP) &u_mt_right MOD TAP
 #define U_LT(LAYER, TAP) &u_lt LAYER TAP
